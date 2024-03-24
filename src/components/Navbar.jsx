0f461c1ce8308d724680b1b382d7d930cf63a927
Sujot-Sasane <sujot31kamble@gmail.com>
2024-03-24 19:32:08 +0530

    Merge remote-tracking branch 'origin/main'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Stack, Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import { Stack } from '@mui/material';
 
 import { logo } from "../utils/constants";
 import { SearchBar } from "./";
